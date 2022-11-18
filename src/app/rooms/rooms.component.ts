c63f4600ee3977427be17e18e866d4dc3b80d593
MRiabov <maksymriabov2004@gmail.com>
2022-11-18 22:35:22 +0100

    Changed rooms component

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "../data.service";
+import {Room} from "../model/Room";
 
 @Component({
   selector: 'app-rooms',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomsComponent implements OnInit {
 
-  constructor() { }
+  rooms: Array<Room>
+
+  constructor(private dataService:DataService) {
+    this.rooms = dataService.rooms;
+  }
 
   ngOnInit(): void {
+    console.log(this.rooms)
   }
 
 }
